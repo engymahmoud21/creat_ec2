a09695e5ce740622ff4ecfbbfcb4ce0747c5fb2a
engymahmoud21 <engy.abuelhassan21@outlook.com>
2024-08-08 22:44:54 +0300

    add var cred

--- variables.tf
+++ variables.tf
@@ -27,3 +27,14 @@ variable "key_name" {
   type        = string
 }
 
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
