2e3d0f02af0389030a63494c10a6446d9b2e7f70
engymahmoud21 <engy.abuelhassan21@outlook.com>
2024-08-29 15:57:33 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-weast-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4"
+    }
+  }
 }
