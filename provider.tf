240c64631b5da93924447e47789492b9e01fa097
engymahmoud21 <engy.abuelhassan21@outlook.com>
2024-08-29 16:02:04 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-weast-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = var.aws_region
+  #region     = var.aws_region
 }
 terraform {
   required_providers {
